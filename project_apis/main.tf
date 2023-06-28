f753d4f7e38c746cd5105a1666a1ff2ac6bf9223
godstaker <alvin.vassoodaven@gmail.com>
2023-06-28 10:22:20 +0100

    Update main.tf
    correcting main.tf, services

--- project_apis/main.tf
+++ project_apis/main.tf
@@ -1,4 +1,4 @@
 resource "google_project_services" "project" {
-  project = var.project_id
-  services   = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com"]
+  project  = var.project_id
+  services = var.services
 }
