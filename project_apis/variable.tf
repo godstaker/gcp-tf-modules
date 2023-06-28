a5472323d289c0f7c1c6ef69835aa78f4ce594a0
godstaker <alvin.vassoodaven@gmail.com>
2023-06-28 10:30:33 +0100

    Update variable.tf
    added vars region and zone

--- project_apis/variable.tf
+++ project_apis/variable.tf
@@ -7,3 +7,15 @@ variable "services" {
   description = "list of apis to be enabled eg:iam.googleapis.com, cloudresourcemanager.googleapis.com"
   type        = list
 }
+
+variable "region" {
+  description = "region of deployment"
+  type        = string
+  default     = "europe-west2"
+}
+
+variable "zone" {
+  description = "zone for deployment"
+  type        = string
+  default     = "europe-west2a"
+}
